a05637bb54c388745100b39fab94c9402aa0aad8
asheesh217 <snehashishghosh60@gmail.com>
2026-06-06 14:05:41 +0530

    Remove temporary file movement scripts

--- move.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-move src frontend\
-move package.json frontend\
-move package-lock.json frontend\
-move vite.config.js frontend\
-move index.html frontend\
-move node_modules frontend\
--- move2.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-mkdir frontend
-move src frontend\
-move package.json frontend\
-move package-lock.json frontend\
-move vite.config.js frontend\
-move index.html frontend\
-move node_modules frontend\
-del move2.bat
